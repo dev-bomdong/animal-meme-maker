87a9b6696273dcf40c55af332921524966d64372
dev-bomdong <dongdh02076@gmail.com>
2023-09-27 16:43:54 +0900

    feat: image 변경 및 text 입력 구현

--- src/assets/images/images.ts
+++ src/assets/images/images.ts
@@ -3,15 +3,21 @@ export const Images = [
     id: 0,
     src: 'src/assets/images/baby_penguin.png',
     name: '걸어가는 펭귄',
+    positionX: 240,
+    positionY: 50,
   },
   {
     id: 1,
     src: 'src/assets/images/thinking_puppy.png',
     name: '생각하는 강아지',
+    positionX: 30,
+    positionY: 100,
   },
   {
     id: 2,
     src: 'src/assets/images/punch_hamster.png',
     name: '주먹왕 햄스터',
+    positionX: 230,
+    positionY: 250,
   },
 ]
--- src/pages/mainPage.tsx
+++ src/pages/mainPage.tsx
@@ -3,41 +3,32 @@ import { useEffect, useRef, useState } from 'react'
 import { Images } from '../assets/images/images'
 
 const MainPage = () => {
-  const canvasRef = useRef(null)
+  const canvasRef: React.RefObject<HTMLCanvasElement> = useRef(null)
   const [canvasText, setCanvasText] = useState<string>('')
-
-  const drawTextOnCanvas = () => {
-    const context =  canvasRef.current.getContext('2d');
-    context.fillText(canvasText, 10, 10);
-  }
-
-
-  const updateImageOnCanvas = () => {
-    const canvas = canvasRef.current;
-    const imageSrc = 'src/assets/images/baby_penguin.png';
-    const context =  canvas.getContext('2d');
-
-    const image = new Image()
-    image.src = imageSrc;
-
-    image.onload = () => {
-      context.drawImage(image, 0, 0, 400, 400)
-    }
-  }
+  const [textPosition, setTextPosition] = useState({
+    x: Images[0].positionX,
+    y: Images[0].positionY,
+  })
+  const [selectedImage, setSelectedImage] = useState(Images[0].src)
 
   const updateCanvas = () => {
-    if (!canvasRef) {
-      return
+    if (canvasRef.current) {
+      const context: CanvasRenderingContext2D =
+        canvasRef.current.getContext('2d')
+
+      const image = new Image()
+      image.src = selectedImage
+      image.onload = () => {
+        context.drawImage(image, 0, 0, 400, 400)
+        context.font = '20px Dotum'
+        context.fillText(canvasText, textPosition.x, textPosition.y)
+      }
     }
-    drawTextOnCanvas()
-    updateImageOnCanvas();
   }
 
   useEffect(() => {
     updateCanvas()
-  }, [canvasText])
-
-
+  }, [canvasText, selectedImage, textPosition])
 
   return (
     <Container>
@@ -46,16 +37,26 @@ const MainPage = () => {
       </Header>
 
       <Body>
-        <select>
+        <select
+          onChange={(e) => {
+            setTextPosition({
+              x: Images[e.target.selectedIndex].positionX,
+              y: Images[e.target.selectedIndex].positionY,
+            })
+            setSelectedImage(e.target.value)
+            setCanvasText('')
+          }}
+        >
           {Images.map((item: any) => (
             <option value={item.src}>{item.name}</option>
           ))}
         </select>
         <canvas id="canvas" ref={canvasRef} width={400} height={400} />
         <Input
+          value={canvasText}
           placeholder={'동물이 할 말을 적어주세요'}
-          onKeyUp={(e) => {
-          setCanvasText(e.target.value);
+          onChange={(e) => {
+            setCanvasText(e.target.value)
           }}
         />
         <DownloadButton>다운로드</DownloadButton>
